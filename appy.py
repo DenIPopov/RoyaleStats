a38976c6ece405cda5be81a8996d9f53c288e3dc
DenIPopov <denis.popov33@icloud.com>
2026-06-11 01:50:02 +0800

    .

--- appy.py
+++ appy.py
@@ -6,7 +6,7 @@ app = Flask(__name__,
 
 @app.route('/')
 def main():
-    return render_template('main.html', active_page='main')
+    return render_template('index.html', active_page='main')
 
 @app.route('/player')
 def player():
